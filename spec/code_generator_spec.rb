97fe92cc7b9ce68d34dc002f85475db8e6577ea5
John Gibson <jg20019@gmail.com>
2021-02-04 12:42:05 -0500

    Create MockIOStream to test code_generator

--- spec/code_generator_spec.rb
+++ spec/code_generator_spec.rb
@@ -1,14 +1,26 @@
 require './lib/node.rb' 
 require './lib/code_generator.rb' 
 
+class MockIOStream 
+  attr_reader :lines
+
+  def initialize()
+    @lines = []
+  end
+
+  def puts(s)  
+    @lines << s
+  end 
+end 
+
 describe CodeGenerator do 
   describe "#generate" do 
     it "generates code for a single number expression" do 
       tree = IntegerNode.new(1)
-      emitter = Emitter.new
-      generator = CodeGenerator.new(emitter)
+      iostream = MockIOStream.new
+      generator = CodeGenerator.new(iostream) 
       generator.generate(tree)
-      expect(emitter.lines[0]).to eql("\tMOVE #1, D0")
+      expect(iostream.lines[0]).to eql("\tMOVE #1, D0")
     end
   end 
 end
