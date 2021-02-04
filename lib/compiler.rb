465c1dddc089667e3ea7f2569cb197fcb1e58318
John Gibson <jg20019@gmail.com>
2021-02-04 04:27:05 -0500

    Add Compiler

---
+++ lib/compiler.rb
@@ -0,0 +1,22 @@
+require './lib/lexer.rb' 
+require './lib/parser.rb' 
+require './lib/emitter.rb'
+require './lib/code_generator.rb'
+
+class Compiler
+  def initialize(source) 
+    @source = source
+  end
+
+  def compile 
+    tokens = Lexer.new(@source).tokenize
+    tree = Parser.new(tokens).parse
+    emitter = Emitter.new
+    code_generator = CodeGenerator.new(emitter) 
+    code_generator.generate(tree) 
+    puts emitter.to_s
+  end
+end
+
+compiler = Compiler.new("1") 
+compiler.compile
