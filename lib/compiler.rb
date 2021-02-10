fded1c7a5f100da32b275536f6c0e1951f1c5f50
John Gibson <jg20019@gmail.com>
2021-02-10 11:41:26 -0500

    Make Compiler.compile a classmethod

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -3,13 +3,10 @@ require './lib/parser.rb'
 require './lib/code_generator.rb'
 
 class Compiler
-  def initialize(source) 
-    @source = source
-  end
-
-  def compile 
-    tokens = Lexer.new(@source).tokenize
+  def self.compile(source)
+    tokens = Lexer.new(source).tokenize
     tree = Parser.new(tokens).parse
     CodeGenerator.new().generate(tree) 
   end
 end
+
